ad7cc036d69699e419cc708b316a1a94968a5f55
Thomas Miller <git@me.tmiller.dev>
2023-03-17 11:23:27 -0500

    Configure experimental features of nix.

--- init-darwin.sh
+++ init-darwin.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+# Install Nix
 sh <(curl -L https://nixos.org/nix/install)
+
+# Configure Nix
+mkdir -p ~/.config/nix
+echo 'experimental-features = nix-command flakes' > ~/.config/nix/nix.conf
