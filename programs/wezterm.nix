aa123e70479ef40e17bbd27f4755d40b324d8640
Thomas Miller <git@me.tmiller.com>
2023-04-11 16:37:28 -0500

    Fix option as meta key on MacOS

--- programs/wezterm.nix
+++ programs/wezterm.nix
@@ -9,6 +9,8 @@ in
       font_size = ${fontSize},
       color_scheme = 'Gruvbox dark, medium (base16)',
       enable_tab_bar = false,
+      send_composed_key_when_left_alt_is_pressed = false,
+      send_composed_key_when_right_alt_is_pressed = false,
     }
   '';
 }
