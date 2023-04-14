0ec60cc8ed2f491bcf726e1675e11c250603e482
Thomas Miller <git@me.tmiller.com>
2023-04-14 09:24:50 -0500

    disable wayland

--- programs/wezterm.nix
+++ programs/wezterm.nix
@@ -11,6 +11,8 @@ in
       enable_tab_bar = false,
       send_composed_key_when_left_alt_is_pressed = false,
       send_composed_key_when_right_alt_is_pressed = false,
+      enable_wayland = false,
+      front_end = 'WebGpu',
     }
   '';
 }
