4afe01ac29ad553b6bbc7843f1fc75017b18ad20
Thomas Miller <git@me.tmiller.com>
2023-09-08 13:53:35 -0500

    Fix fzf in Neovim

--- programs/fzf.nix
+++ programs/fzf.nix
@@ -1,36 +1,35 @@
 { config }:
+let
+  excludes = [
+    "--exclude .git"
+    "--exclude .terraform"
+    "--exclude .terragrunt-cache"
+    "--exclude node_modules"
+    "--exclude vendor"
+    "--exclude elm-stuff"
+    "--exclude deps"
+    "--exclude _build"
+    "--exclude target"
+    "--exclude pkg"
+    "--exclude Library"
+    "--exclude .Trash"
+  ];
+in
 {
+
   enable = true;
-  fileWidgetCommand = builtins.concatStringsSep " " [
+  defaultCommand = builtins.concatStringsSep " " ([
+    "fd"
+    "--type file"
+  ] ++ excludes);
+
+  fileWidgetCommand = builtins.concatStringsSep " " ([
+    "fd"
+    "--type file"
+  ] ++ excludes);
+
+  changeDirWidgetCommand = builtins.concatStringsSep " " ([
     "fd"
     "--type directory"
-    "--exclude .git"
-    "--exclude .terraform"
-    "--exclude .terragrunt-cache"
-    "--exclude node_modules"
-    "--exclude vendor"
-    "--exclude elm-stuff"
-    "--exclude deps"
-    "--exclude _build"
-    "--exclude target"
-    "--exclude pkg"
-    "--exclude Library"
-    "--exclude .Trash"
-  ];
-  changeDirWidgetCommand = builtins.concatStringsSep " " [
-    "fd"
-    "--type directory"
-    "--exclude .git"
-    "--exclude .terraform"
-    "--exclude .terragrunt-cache"
-    "--exclude node_modules"
-    "--exclude vendor"
-    "--exclude elm-stuff"
-    "--exclude deps"
-    "--exclude _build"
-    "--exclude target"
-    "--exclude pkg"
-    "--exclude Library"
-    "--exclude .Trash"
-  ];
+  ] ++ excludes);
 }
