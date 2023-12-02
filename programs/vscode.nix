ee90da474b28d0d1d495d624bee5904e425a416e
Thomas Miller <git@me.tmiller.com>
2023-12-02 12:17:36 -0600

    Configure Visual Studio Code

--- programs/vscode.nix
+++ programs/vscode.nix
@@ -1,4 +1,21 @@
 {config, pkgs}:
 {
   enable = true;
+  extensions = with pkgs.vscode-extensions; [
+    vscodevim.vim
+    github.copilot
+    github.copilot-chat
+  ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+    {
+      name = "aws-toolkit-vscode";
+      publisher = "amazonwebservices";
+      version = "2.1.0";
+      sha256 = "sha256-09JzYBQeHOptryUjSWFBY/k4CpH9UD6yI/xXwN8zU0E=";
+    }
+  ];
+  userSettings = {
+    "files.trimTrailingWhitespace" = true;
+    "editor.formatOnSave" = true;
+    "git.openRepositoryInParentFolders" = "never";
+  };
 }
