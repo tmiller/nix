47f5d29959d802263b2b4256de69cbfcdc2a71b5
Thomas Miller <git@me.tmiller.dev>
2024-01-16 18:56:00 -0600

    Fix gpg-agent configuration
    Setting SSH_AUTH_SOCK without checking if it first exists breaks
    ssh-agent forwarding. This is set properly if you configure gpg-agent.
    This fixes that and move the config out to the more generalized area.
    Not sure if it works on Darwin yet.

--- systems/nixos.nix
+++ systems/nixos.nix
@@ -45,13 +45,6 @@
   '';
 
   services.gpg-agent = {
-    enable = true;
-    enableExtraSocket = true;
-    enableSshSupport = true;
     pinentryFlavor = "gnome3";
-    sshKeys = [
-      "7F7CF78A1316240108E5837CAC4A50B589E2CDEA"
-      "F717FDE1B01A67A6CAE08303024B28E04483C320"
-    ];
   };
 }
